f8e1d0564c9a9987a6a8a04e5b9f574e337742be
LaoBing_Fung <FLB_B401@163.com>
2019-01-11 17:08:09 +0800

    update method of completing a todo item

--- js/display_events.js
+++ js/display_events.js
@@ -27,24 +27,6 @@ function displayLeftItems(itemArr) {
 
 function displayList(itemArr, ul) {
   itemArr.forEach((curItem) => displayNewItem(curItem, ul))
-  // {
-  //   let li = document.createElement('li');
-  //   let checkbox = document.createElement('input');
-  //   checkbox.setAttribute('type', 'checkbox');
-  //   let content = document.createElement('label');
-  //   content.innerText = curItem.content;
-  //   if (curItem.completed === true) {
-  //     content.classList.add('completed');
-  //   }
-  //   let btn = document.createElement('button');
-  //   btn.innerText = 'X';
-  //   btn.classList.add('btn_hide');
-  //   btn.classList.add('delete_btn');
-  //   li.appendChild(checkbox);
-  //   li.appendChild(content);
-  //   li.appendChild(btn);
-  //   ul.appendChild(li);
-  // })
 }
 
 function add(event) {
@@ -87,4 +69,28 @@ function displayClearCompleted(itemArr) {
   } else {
     clearBtn.classList.add('btn_hide');
   }
+}
+
+function complete(event) {
+  let target = event.target;
+  let inedx;
+  let checkbox;
+  let label;
+  if (target.nodeName === 'LI') {
+    index = todoList.list.findIndex((curItem) => curItem.content === target.children[1].innerText && curItem.completed === false);
+    label = target.children[1];
+    checkbox = target.children[0];
+  }
+  if (target.nodeName === 'INPUT' || target.nodeName === 'LABEL') {
+    index = todoList.list.findIndex((curItem) => curItem.content === target.parentElement.children[1].innerText && curItem.completed === false);
+    label = target.parentElement.children[1];
+    checkbox = target.parentElement.children[0];
+  }
+  todoList.complete(index);
+  label.classList.add('completed');
+  checkbox.setAttribute('checked', true);
+  checkbox.setAttribute('disabled', true);
+  let activeList = todoList.getActive();
+  displayLeftItems(activeList);
+  displayClearCompleted(activeList);
 }
